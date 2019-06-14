b939581b08efbdd3fb7353f83160a126f212e711
Michał Sarzała <sarzal666@gmail.com>
2019-06-14 02:04:56 +0200

    added slider

---
+++ js/myscripts.js
@@ -0,0 +1,23 @@
+        (function ($) {
+
+            $(document).ready(function () {
+
+                // ---------------- Main slider ----------------
+                $('.slider').slick({
+                    autoplay: true,
+                    autoplaySpeed: 1500,
+                    arrows: false,
+                    dots: false,
+                    infinite: true,
+                    speed: 500,
+                    slidesToShow: 1,
+                    slideToScroll: 1
+
+
+
+                });
+                // -------------- Main slider END --------------
+
+            }); //document ready
+
+        })(jQuery);
